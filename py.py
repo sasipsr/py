ac1ffb371187a4e30c894d55a69465ce577e501a
sasipsr <33392014+sasipsr@users.noreply.github.com>
2021-06-07 10:39:13 +0530

    Create py.py

---
+++ py.py
@@ -0,0 +1,14 @@
+username = 'Polly1220'
+
+password = 'Bob'
+
+input = 'password'
+
+if userInput == username:
+    a=input("Password?\n")   
+    if a == password:
+        print("Welcome!")
+    else:
+        print("That is the wrong password.")
+else:
+    print("That is the wrong username.")
