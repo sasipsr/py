258a9aa0136407564fe54e1bcaa0a53d594372c6
sasipsr <33392014+sasipsr@users.noreply.github.com>
2021-06-07 10:48:43 +0530

    Update py.py

--- py.py
+++ py.py
@@ -1,3 +1,9 @@
-z = 4
-if z % 2 == 0 :
-print("z is even")
+num = 3
+if num > 0:
+    print(num, "is a positive number.")
+print("This is always printed.")
+
+num = -1
+if num > 0:
+    print(num, "is a positive number.")
+print("This is also always printed.")
